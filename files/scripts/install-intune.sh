05aa03120603c2b6edb281d3311db3b36037e28d
L0g0ff <thomas@wapnet.nl>
2026-03-11 15:31:16 +0100

    chore: add --nogpgcheck option to dnf install for compatibility with Microsoft repo

--- files/scripts/install-intune.sh
+++ files/scripts/install-intune.sh
@@ -9,7 +9,9 @@ curl -o /etc/yum.repos.d/microsoft-rhel9-prod.repo \
 # java-11-openjdk is required by microsoft-identity-broker but unavailable in
 # Fedora 43 repositories. Packages sourced from Fedora 41 koji:
 # https://kojipkgs.fedoraproject.org/packages/java-11-openjdk/11.0.29.0.7/1.fc41/x86_64/
-dnf install -y \
+# --nogpgcheck is required because microsoft-identity-diagnostics has no digest
+# in the Microsoft repo metadata, causing DNF5 to reject the transaction.
+dnf install -y --nogpgcheck \
     https://github.com/ThomasInfield/KompassOS-Managed/releases/download/managed-deps/java-11-openjdk-headless-11.0.29.0.7-1.fc41.x86_64.rpm \
     https://github.com/ThomasInfield/KompassOS-Managed/releases/download/managed-deps/java-11-openjdk-11.0.29.0.7-1.fc41.x86_64.rpm \
     microsoft-identity-broker \
